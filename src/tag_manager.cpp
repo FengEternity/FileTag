feceb51af15df30282fd34601e61bd6b0ddbd570
Monty __L <b21060414@njupt.edu.cn>
2024-07-11 17:34:05 +0800

    Add build directory to .gitignore

---
+++ src/tag_manager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Monty-Lee  on 24-7-11.
+//
+
+#include "tag_manager.h"
---
+++ src/tag_manager.h
@@ -0,0 +1,16 @@
+//
+// Created by Monty-Lee  on 24-7-11.
+//
+
+#ifndef FILETAG_TAG_MANAGER_H
+#define FILETAG_TAG_MANAGER_H
+
+
+
+class tag_manager {
+
+};
+
+
+
+#endif //FILETAG_TAG_MANAGER_H
