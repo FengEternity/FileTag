27747b77b8e88b4664811e68b1a08671762c9908
Monty __L <b21060414@njupt.edu.cn>
2024-07-17 10:06:52 +0800

    学习理解主函数

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,24 @@
 #include <QFile>
 #include <QMainWindow>
 #include "mainwindow.h"
+#include "Logger.h"
 
 void applyStyleSheet(QApplication &app) {
     QFile file(":/stylesheet.qss");
     if (!file.exists()) {
-        qWarning("未找到样式表");
+        // qWarning("未找到样式表");
+        Logger::instance().log("未找到样式表");
         return;
     }
     if (file.open(QFile::ReadOnly | QFile::Text)) {
+        Logger::instance().log("加载样式表");
         QTextStream stream(&file);
         QString styleSheet = stream.readAll();
         app.setStyleSheet(styleSheet);
         file.close();
     } else {
-        qWarning("无法加载样式表");
+        // qWarning("无法加载样式表");
+        Logger::instance().log("无法加载样式表");
     }
 }
 
